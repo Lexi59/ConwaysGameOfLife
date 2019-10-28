b429e64f70dea82fbb25c37a11454b2823e62128
Lexi59 <trip9919@gmail.com>
2019-10-28 12:38:19 -0500

    Initial Commit

---
+++ ConwaysGameOfLife/CellClass.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConwaysGameOfLife
+{
+    class Cell
+    {
+        //constructor
+        public Cell(int _num)
+        {
+            num = _num;
+            status = 0;
+        }
+        //fields
+        private int[] history;
+        public int status;
+        public int aliveNeighbors;
+        public int num;
+
+        //methods
+        public void checkNeighbors()
+        {
+            int neighborsAlive = 0;
+            int[] neighborNum = { num - 1, num - 11, num - 10, num - 9, num + 1, num + 11, num + 10, num + 9 };
+            for (int i = 0; i < neighborNum.Length; i++)
+            {
+                if (validNeighbor(num, neighborNum[i]))
+                {
+                    Cell neighbor = Form1.Cells.Find(x => x.num == neighborNum[i]);
+                    if (neighbor == null)
+                    {
+
+                    }
+                    else if(neighbor.status == 1)
+                    {
+                        neighborsAlive++;
+                    }
+                }
+            }
+            aliveNeighbors = neighborsAlive;
+        }
+        private bool validNeighbor(int current, int checking)
+        {
+            //left column
+            if (current % 10 == 1 && checking % 10 == 0)
+            {
+                return false;
+            }
+            //top or bottom row
+            if (checking < 1 || checking > 100)
+            {
+                return false;
+            }
+            //right column
+            if (current % 10 == 1 && checking % 10 == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+        public void changeStatus()
+        {
+            if(status == 0 && aliveNeighbors == 3)
+            {
+                status = 1;
+            }
+            if (status == 1 && aliveNeighbors <= 1)
+            {
+                status = 0;
+            }
+            if(status == 1 && aliveNeighbors >= 4)
+            {
+                status = 0;
+            }
+            if (status == 1 && aliveNeighbors == 2)
+            {
+                status = 1;
+            }
+            if (status == 1 && aliveNeighbors == 3)
+            {
+                status = 1;
+            }
+        }
+    }
+}
---
+++ ConwaysGameOfLife/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ConwaysGameOfLife
+{
+    public partial class Form1 : Form
+    {
+        public static List<Button> buttons = new List<Button>();
+        private static List<Cell> cells = new List<Cell>();
+        internal static List<Cell> Cells { get => cells; set => cells = value; }
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 1; i < 100; i++)
+            {
+                buttons.Add((Button)this.Controls.Find(("button" + i), true)[0]);
+                Cells.Add(new Cell(i));
+            }
+            Random random = new Random();
+            for (int i = 0; i < 50; i++)
+            {
+                int num = random.Next(0, cells.Count - 1);
+                cells[num].status = 1;
+                buttons[num].BackColor = Color.Red;
+            }
+            System.Windows.Forms.Timer timer = new Timer();
+            timer.Interval = 2000;
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < cells.Count; i++)
+            {
+                cells[i].checkNeighbors();
+                //buttons[i].Text = cells[i].aliveNeighbors.ToString();
+                cells[i].changeStatus();
+                if (cells[i].status == 1)
+                {
+                    buttons[i].BackColor = Color.Red;
+                }
+                else
+                {
+                    buttons[i].BackColor = Color.White;
+                }
+            }
+        }
+    }
+}
